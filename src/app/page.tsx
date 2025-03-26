90343bce063dff74d669e06fb3c3f2fbe568df23
Pavani Racham <g23ai2032@iitj.ac.in>
2025-03-26 14:21:04 +0530

    Delete src directory

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import IPEConsole from "@/components/IPEConsole"
-
-export default function Home() {
-  return (
-    <main className="min-h-screen bg-gradient-to-br from-gray-900 to-gray-800">
-      <IPEConsole />
-    </main>
-  )
-} 
